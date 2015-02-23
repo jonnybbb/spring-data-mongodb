018c7d6b973477030b6ce7fb27460af2c8d73193
Johannes Buehler <jonnybbb@gmail.com>
2015-02-23 18:47:40 +0100

    updated branch to latest spring-data version

--- spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/mapping/event/AfterDeleteEvent.java
+++ spring-data-mongodb/src/main/java/org/springframework/data/mongodb/core/mapping/event/AfterDeleteEvent.java
@@ -1,21 +1,3 @@
-<<<<<<< Temporary merge branch 1
-package org.springframework.data.mongodb.core.mapping.event;
-
-/**
- * Event invoked after document is deleted with methods:
- * <ul>
- *     <li>{@link org.springframework.data.mongodb.core.MongoOperations#remove(Object)}</li>
- *     <li>{@link org.springframework.data.mongodb.core.MongoOperations#remove(Object, String)}</li>
- * </ul>
- *
- * @param <T> removed object
- *
- * @author Maciej Walkowiak
- */
-public class AfterDeleteEvent<T> extends MongoMappingEvent<T> {
-	public AfterDeleteEvent(T source) {
-		super(source, null);
-=======
 /*
  * Copyright 2013 by the original author(s).
  *
